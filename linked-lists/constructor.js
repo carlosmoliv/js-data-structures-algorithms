a2281ed49fccfc46821fe168bfaf1f4c64c714e2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-10-14 12:49:20 -0300

    feat: finish constructor overview to create linked-lists

--- linked-lists/constructor.js
+++ linked-lists/constructor.js
@@ -1,12 +1,32 @@
+// In order to create a node
+// What is being created by the node class is:
+// {value: 4, next:null}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   // Creates a new node
-  constructor(value) {}
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
   // Creates a new node and adds it to the end of the list
   push(value) {}
+
   // Creates a new node and adds it to the beginning of the list
   unshift(value) {}
+
   // Creates a new node and adds it to whatever index is passed
   insert(index, value) {}
 }
 
-// So, they all creates a node
+// Basically creates a new node with head and tail pointing to it
+const myLinkedList = new LinkedList(4);
+console.log(myLinkedList);
