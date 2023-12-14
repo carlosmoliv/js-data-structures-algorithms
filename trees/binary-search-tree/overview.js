b45a3d7cc8e8409ff16c13fe7b2a6fb8032beb79
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-12-14 20:21:38 -0300

    feat: add minValueNode method to BST class

--- trees/binary-search-tree/overview.js
+++ trees/binary-search-tree/overview.js
@@ -3,7 +3,7 @@
 // conquer and divide apllied to binary search tree
 // worst scenario is O(n) -> when the tree is not balanced
 // lookup, insert and remove is O(log n)
-// now, dcomparing with linked lists, where lookup is O(n), insert is O(1) and remove is O(n)
+// now, comparing with linked lists, where lookup is O(n), insert is O(1) and remove is O(n)
 // insert is better for a linked list, but lookup and remove is better for a binary search tree
 // lose efficiency inserting in the binary search tree, but gain efficiency in lookup and remove
-//  insert count for duplications
+// insert count for duplications
