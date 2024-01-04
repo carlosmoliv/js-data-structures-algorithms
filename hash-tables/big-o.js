1e76569869e1f9439486f0567ef4806e7fad364d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-03 22:32:36 -0300

    docs: add Big O analysis for Hash Tables

---
+++ hash-tables/big-o.js
@@ -0,0 +1,14 @@
+/**
+ * Big O of Hash Tables---------------------------------------
+ * The hash method itself is O(1) constant time
+ * The set method is O(1) constant time
+ * The get method is O(1) constant time. Only by the key is O(1)
+ */
+
+// The hash fucntion is going to be very efficient at randomizing the addresses where things get assigned
+
+/**
+ * Both Insert and Lookup by key in a Hash Table is O(1)
+ * Since a Hash Table is O(1) for Insert and Lookup it is always better than a Binary Search Tree
+ * Looking up a value in a Hash Table is O(1)
+ */
